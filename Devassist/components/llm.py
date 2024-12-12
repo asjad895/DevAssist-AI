b707faf5fb05ff2ae4b74795a8ad9633d0409613
Md Asjad <109430048+asjad895@users.noreply.github.com>
2024-12-12 19:21:56 +0000

    added base llm code

--- Devassist/components/llm.py
+++ Devassist/components/llm.py
@@ -1,7 +1,40 @@
 from dotenv import load_dotenv
-load_dotenv()
 import os
+from groq import AsyncGroq
+from typing import List,Dict,Union
+load_dotenv()
+
 
 api_key = os.getenv('LLM_API_KEY')
 
+
+class LLM:
+    def __init__(self,key : str ,model : str = "") -> None:
+        if not key:
+            raise KeyError()
+        else:
+            self.key = key
+
+        self.model = model
+
+        self.client = AsyncGroq(api_key=self.key)
+
+
+    
+    async def get_non_stream_response(self,query : str,chat_history:List[Dict[str,str]],system_message : str)->Dict:
+
+        if not isinstance(query,str) or not isinstance(chat_history,List) or not isinstance(system_message,str):
+            return {
+                'error':"Invalid Input"
+            }
+        
+        messages = [{'role':'system','content':system_message}]+chat_history + [{'role':'user','content':query}]
+        
+        chat_completion = await self.client.chat.completions.create(
+            messages = messages, # type: ignore
+            model = self.model
+        )
+        print(chat_completion.choices[0].message.content)
+
+        return chat_completion.choices[0].message.to_dict()
 
--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse 
-from Devassist.components import Loadfiles
+from Devassist.components import Loadfiles,llm
+from dotenv import load_dotenv
 import json
+import os
+load_dotenv('/workspaces/DevAssist-AI/.env')
 
 
 app = FastAPI()
+
+key = os.getenv('LLM_API_KEY')
+client = llm.LLM(key = key,model = 'mixtral-8x7b-32768')
 
 @app.post('/chat/')
 async def chat(request:Request)->JSONResponse:
@@ -16,8 +22,15 @@ async def chat(request:Request)->JSONResponse:
         download_data = Loadfiles.GitHubRepoInput(repo_url=input_data['github_url'])
 
         await Loadfiles.download_github_repo(download_data)
-        return JSONResponse({'status':'success'})
+
+        query = input_data.get('query')
+        response = await client.get_non_stream_response(
+            query = query,
+            chat_history = [],
+            system_message = "You are as assistant."
+        )
+        print(response)
+        return JSONResponse({'status':'success','response':response['content']})
     except Exception as e:
         print(f'Error at json parsing \n{e}')
         return JSONResponse(status_code=400,content={'error':'Bad request','detail':str(e)})
-
